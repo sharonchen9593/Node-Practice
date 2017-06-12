0e53b029e8dd8a7f8d4ec5f9dc5a2daa583204db
sharonchen9593 <sharonchen9593@gmail.com>
2017-06-12 15:20:18 -0700

    refactored more

--- Event Emitter/app.js
+++ Event Emitter/app.js
@@ -1,14 +1,15 @@
 var Emitter = require('events');
+var eventConfig = require('./config').events
 
 var emtr = new Emitter();
 
-emtr.on('greet', function() {
+emtr.on(eventConfig.GREET, function() {
 	console.log('Somewhere, someone said hello.');
 });
 
-emtr.on('greet', function() {
+emtr.on(eventConfig.GREET, function() {
 	console.log('A greeting occurred!');
 });
 
 console.log('Hello!');
-emtr.emit('greet');
+emtr.emit(eventConfig.GREET);
