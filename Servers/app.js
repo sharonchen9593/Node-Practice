caf04c72f2c0d81129322b59a9195264b5ae214d
sharonchen9593 <sharonchen9593@gmail.com>
2017-06-13 01:23:21 -0700

    outputting json

--- Servers/app.js
+++ Servers/app.js
@@ -3,9 +3,12 @@ var fs = require('fs'); // deliver html to app.js
 
 http.createServer(function(request, response) {
 
-  response.writeHead(200, {'Content-Type': 'text/html'}); // status code, content type
-  fs.createReadStream(__dirname + '/index.html').pipe(response); // pipe html to response stream
-
+  response.writeHead(200, {'Content-Type': 'application/json'}); // status code, content type
+  var obj = {
+    firstname: 'John',
+    lastname: 'Doe'
+  };
+  response.end(JSON.stringify(obj));
 
 }).listen(1337, '127.0.0.1'); // port, ip address. in browser, look at localhost:1337
 
@@ -15,3 +18,9 @@ http.createServer(function(request, response) {
 // var message = "Hello world...";
 // html = html.replace('{Message}', message)
 // response.end(html); // send response back
+
+
+
+
+
+// fs.createReadStream(__dirname + '/index.html').pipe(response); // pipe html to response stream
