a4c18105e781b424e4d095604d62364bf4092d63
sharonchen9593 <sharonchen9593@gmail.com>
2017-06-13 01:39:30 -0700

    added routing

--- Servers/app.js
+++ Servers/app.js
@@ -3,12 +3,20 @@ var fs = require('fs'); // deliver html to app.js
 
 http.createServer(function(request, response) {
 
-  response.writeHead(200, {'Content-Type': 'application/json'}); // status code, content type
-  var obj = {
-    firstname: 'John',
-    lastname: 'Doe'
-  };
-  response.end(JSON.stringify(obj));
+  //routing
+  if (request.url === '/') { // this will come up if no endpoint
+    fs.createReadStream(__dirname + '/index.html').pipe(response); // pipe html to response stream
+  } else if (request.url === '/api') { // this will come up if endpoint is /api
+    response.writeHead(200, {'Content-Type': 'application/json'}); // status code, content type
+    var obj = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+    response.end(JSON.stringify(obj));
+  } else {
+    response.writeHead(404);
+    response.end()
+  }
 
 }).listen(1337, '127.0.0.1'); // port, ip address. in browser, look at localhost:1337
 
