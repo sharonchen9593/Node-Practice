77e5cf4984f298ab0c8f1972f8e77190de66a9d2
sharonchen9593 <sharonchen9593@gmail.com>
2017-06-13 21:49:26 -0700

    deleted npm modules to save space

---
+++ npm/app.js
@@ -0,0 +1,6 @@
+// npm init to create a package.json file
+// npm install moment --save to download moment package
+
+// the ^ in front of version means that it's ok for npm to update anything in version 2. but dont update to version 3
+
+var moment = require('moment') // dont need ./ because app knows to look in node modules folder
